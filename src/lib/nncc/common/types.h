e8ce9833a616daad785a182dfbf7a080150ab48b
apnkv <alexpnkv@gmail.com>
2022-06-20 09:55:59 +0100

    Use Folly string, vector and SPSC queue

---
+++ src/lib/nncc/common/types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <folly/FBString.h>
+#include <folly/FBVector.h>
+
+namespace nncc {
+
+using string = folly::fbstring;
+
+template <class T, class Allocator = std::allocator<T>>
+using vector = folly::fbvector<T, Allocator>;
+
+}
